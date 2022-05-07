493b92b990c0494b50031c0c7f6b59e53aed2bb4
ro-ze1106 <85288297+ro-ze1106@users.noreply.github.com>
2022-05-07 18:05:12 +0900

    アロー関数

--- src/index.js
+++ src/index.js
@@ -49,8 +49,8 @@
 /**
  * テンプレート文字列
  */
-const name = "tomo";
-const age = 30;
+// const name = "tomo";
+// const age = 30;
 // 「私の名前はtomoです。年齢は30歳です。」
 
 // 従来の方法
@@ -58,5 +58,27 @@ const age = 30;
 // console.log (message1) ;
 
 // テンプレート文字列を用いた方法
-const message2 = `私の名前は${name}です。年齢は${age}です。`;
-console.log(message2);
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+/**
+ * アロー関数
+ */
+// 従来の関数
+// function func1(str) {
+//   return str;
+// }
+const func1 = function (str) {
+  return str;
+};
+console.log(func1("func1です。"));
+
+//　アロー関数
+const func2 = (str) => {
+  return str;
+};
+console.log(func2("func2です。"));
+
+//　処理を単1行で返却する場合は{}とreturnを省略できる
+const func3 = (num1, num2) => num1 + num2;
+console.log(func3(10, 20));
