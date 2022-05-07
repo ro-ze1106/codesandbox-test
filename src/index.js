6b8d079f7c007b3fa00835060377b7570d2933c5
ro-ze1106 <85288297+ro-ze1106@users.noreply.github.com>
2022-05-07 18:29:51 +0900

    分割代入

--- src/index.js
+++ src/index.js
@@ -68,17 +68,40 @@
 // function func1(str) {
 //   return str;
 // }
-const func1 = function (str) {
-  return str;
-};
-console.log(func1("func1です。"));
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1です。"));
 
-//　アロー関数
-const func2 = (str) => {
-  return str;
-};
-console.log(func2("func2です。"));
+// //　アロー関数
+// const func2 = (str) => {
+//   return str;
+// };
+// console.log(func2("func2です。"));
 
-//　処理を単1行で返却する場合は{}とreturnを省略できる
-const func3 = (num1, num2) => num1 + num2;
-console.log(func3(10, 20));
+// //　処理を単1行で返却する場合は{}とreturnを省略できる
+// const func3 = (num1, num2) => num1 + num2;
+// console.log(func3(10, 20));
+
+/**
+ * 分割代入
+ */
+// const myProfile = {
+//   name: "tomo",
+//   age: 30,
+// };
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}です。`;
+// console.log (message1);
+
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}です。`;
+// console.log (message2);
+
+const myProfile = ["tomo", 30];
+
+const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}です。`;
+console.log(message3);
+
+const [name, age] = myProfile;
+const message4 = `名前は${name}です。年齢は${age}です。`;
+console.log(message4);
